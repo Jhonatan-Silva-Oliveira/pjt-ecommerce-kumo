56e049329b816121f5d1795347eb1473d2cec83e
JhonatanSO <jhonerex2012@gmail.com>
2022-12-06 11:58:43 -0300

    adicionado header

--- src/componentes/Topo/index.jsx
+++ src/componentes/Topo/index.jsx
@@ -3,7 +3,23 @@ import './style.css';
 export default function Topo() {
   return (
     <header>
-    
+      <a href="#" className="logo"><img src="../../public/assets/logo.png" alt="logo" /></a>
+      
+      <ul className="navmenu">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Shop</a></li>
+        <li><a href="#">Products</a></li>
+        <li><a href="#">Page</a></li>
+        <li><a href="#">Docs</a></li>
+      </ul>
+      
+      <div className="nav-icon">
+        <a href="#"><i class='bx bx-search'></i></a>
+        <a href="#"><i class='bx bx-user'></i></a>
+        <a href="#"><i class='bx bx-cart'></i></a>
+        
+        <div class='bx bx-menu' id='menu-icon'></div>
+      </div>
     </header>
   )
 }
